e7a26682fdcfa6a79957e0b75a8155758b6e990c
Preston Weber <preston159@gmail.com>
2019-09-21 12:34:03 -0700

    fixed score display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 var timeLeft = 30;
 var intervalId;
+var totalCorrect = 0;
+var femur = $("#femur");
+var ninetySeven = $("#ninetySeven");
+var possum = $("#possum");
+var morpheus = $("#morpheus");
+var submit = $("#submit");
 
 function run() {
     intervalId = setInterval(count, 1000);
@@ -9,27 +15,49 @@ function run() {
 
 function count() {
     timeLeft = timeLeft - 1;
-    console.log(timeLeft);
     $("#time-left").html("<h2>" + timeLeft + "</h2>");
-    if(timeLeft === 0) {
+    if (timeLeft === 0) {
         timeUp();
+        
+        //$("p").append("<p>Your final score is: " + totalCorrect + "/4</p>")
         return false;
-    }
-}
+
+    };
+
+};
 
 
 function timeUp() {
     clearInterval(intervalId);
-    timeLeft = 30;
-    $(".container").empty()
-}
+    console.log(femur[0].checked ,femur);
+
+
+    if (femur[0].checked == true) {
+        totalCorrect++;
+    }
+    if (ninetySeven[0].checked == true) {
+        totalCorrect++;
+    }
+    if (possum[0].checked == true) {
+        totalCorrect++;
+    }
+    if (morpheus[0].checked == true) {
+        totalCorrect++;
+    }
+    $(".container").empty();
+    $(".container").append("<p>Your final score is: " + totalCorrect + "/4</p>")
+    console.log(totalCorrect);
+
+
+
+};
+
+submit.on("click", timeUp)
 
 // create a function that calculates the score and call upon it in the timeUp function
 
 
-function score() {
 
-}
 
 run();
 
